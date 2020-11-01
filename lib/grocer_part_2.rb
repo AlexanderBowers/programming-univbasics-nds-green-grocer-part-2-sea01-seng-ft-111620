84f117ccad954382ba1f8918376d815551f65c17
Alexander Bowers <7319abowers@gmail.com>
2020-11-01 07:03:46 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -32,15 +32,6 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
   total = 0.0
   cart = consolidate_cart(cart)
   cart = apply_coupons(cart, coupons)
